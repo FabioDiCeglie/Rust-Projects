467ea45139a0973e8e881352ab6d2dcf75c6deb4
FabioDiCeglie <fabiod@backbase.com>
2024-09-09 15:49:21 +0200

    feat: start working on the send action and conversation state for the front end

--- ai-chat/src/app.rs
+++ ai-chat/src/app.rs
@@ -2,19 +2,36 @@ use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 
+use crate::model::conversation::{Conversation, Message};
+
 #[component]
 pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
+
+    // Similar to useState in React: `conversation` is the state, and `set_conversation` is the function to update it
+    let(conversation, set_conversation) = create_signal(Conversation::new());
+
+    let send = create_action(move |new_message: &String| {
+        let user_message = Message {
+            text: new_message.clone(),
+            user: true,
+        };
+        set_conversation.update(move |c| {
+            c.messages.push(user_message);
+        });
+
+        // TODO converse
+        async move {}
+    });
 
     view! {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/ai-chat.css"/>
 
-        // sets the document title
         <Title text="Rust Chat AI"/>
-        <ChatArea/>
-        <TypeArea/>
+        <ChatArea conversation />
+        <TypeArea send />
     }
 }
